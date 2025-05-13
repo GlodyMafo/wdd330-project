17cabe9788338eb333197b13192a81a9a5b188ee
Glody Mafo <glodymafokis@gmail.com>
2025-05-13 21:19:15 +0200

    debuging

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,24 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+  let cart = [];
+
+  const storedCart = localStorage.getItem("so-cart");
+
+  if (storedCart) {
+    try {
+      const parsed = JSON.parse(storedCart);
+      cart = Array.isArray(parsed) ? parsed : [parsed]; 
+    } catch (e) {
+      cart = [];
+    }
+  }
+
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
+
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
